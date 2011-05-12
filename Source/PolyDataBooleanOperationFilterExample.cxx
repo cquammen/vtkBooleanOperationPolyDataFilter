7bd9c4d1572783f5d09dfbc2d44d025f59832fc5
Cory Quammen <cquammen@cs.unc.edu>
2011-05-12 11:58:07 -0400

    Modified example to show all three boolean operations.

--- Source/PolyDataBooleanOperationFilterExample.cxx
+++ Source/PolyDataBooleanOperationFilterExample.cxx
@@ -1,5 +1,4 @@
 #include <vtkActor.h>
-#include <vtkImplicitPolyData.h>
 #include <vtkPolyDataBooleanOperationFilter.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkRenderer.h>
@@ -7,26 +6,37 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkSphereSource.h>
 
-int main(int argc, char* argv[])
+vtkActor* GetBooleanOperationActor( double x, int operation )
 {
+  double centerSeparation = 0.15;
   vtkSphereSource *sphere1 = vtkSphereSource::New();
-  sphere1->SetCenter(0.0, -0.25, 0.0);
+  sphere1->SetCenter(-centerSeparation + x, 0.0, 0.0);
 
   vtkSphereSource *sphere2 = vtkSphereSource::New();
-  sphere2->SetCenter(0.0, 0.25, 0.0);
+  sphere2->SetCenter(  centerSeparation + x, 0.0, 0.0);
 
   vtkPolyDataBooleanOperationFilter *boolFilter =
     vtkPolyDataBooleanOperationFilter::New();
-  boolFilter->SetOperationToDifference();
+  boolFilter->SetOperation( operation );
   boolFilter->SetInputConnection( 0, sphere1->GetOutputPort() );
-  boolFilter->SetInputConnection( 1, sphere1->GetOutputPort() );
+  sphere1->Delete();
+  boolFilter->SetInputConnection( 1, sphere2->GetOutputPort() );
+  sphere2->Delete();
 
   vtkPolyDataMapper *mapper = vtkPolyDataMapper::New();
   mapper->SetInputConnection( boolFilter->GetOutputPort() );
+  mapper->ScalarVisibilityOff();
 
   vtkActor *actor = vtkActor::New();
   actor->SetMapper( mapper );
+  mapper->Delete();
 
+  return actor;
+}
+
+
+int main(int argc, char* argv[])
+{
   vtkRenderer *renderer = vtkRenderer::New();
 
   vtkRenderWindow *renWin = vtkRenderWindow::New();
@@ -35,10 +45,25 @@ int main(int argc, char* argv[])
   vtkRenderWindowInteractor * renWinInteractor = vtkRenderWindowInteractor::New();
   renWinInteractor->SetRenderWindow( renWin );
 
-  renderer->AddActor( actor );
+  vtkActor *unionActor = 
+    GetBooleanOperationActor( -2.0, vtkPolyDataBooleanOperationFilter::UNION );
+  vtkActor *intersectionActor =
+    GetBooleanOperationActor(  0.0, vtkPolyDataBooleanOperationFilter::INTERSECTION );
+  vtkActor *differenceActor =
+    GetBooleanOperationActor(  2.0, vtkPolyDataBooleanOperationFilter::DIFFERENCE );
+
+  renderer->AddActor( unionActor );
+  unionActor->Delete();
+  renderer->AddActor( intersectionActor );
+  intersectionActor->Delete();
+  renderer->AddActor( differenceActor );
+  differenceActor->Delete();
 
   renWin->Render();
   renWinInteractor->Start();
+
+  renWinInteractor->Delete();
+  renWin->Delete();
 
   return EXIT_SUCCESS;
 }
